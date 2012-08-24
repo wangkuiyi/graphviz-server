324ba64f28dcd16ac056d49f57490858f057f841
Yi Wang <yi.wang.2005@gmail.com>
2012-08-24 21:57:06 +0800

    Make parse-http-request judge the end of header by empty line.

--- parse-http-request.rkt
+++ parse-http-request.rkt
@@ -13,12 +13,13 @@
 
 ;; Parse headers as an alist.
 (define (parse-headers in header-list)
-  (let ((header (regexp-match #rx"([^:]+): ([^\r]+)\r\n" in)))
-    (if (false? header)
-        header-list
+  (let ((header (regexp-match #rx"((([^:]+): ([^\r]+))|^)\r\n" in)))
+    ;; In usual cases, header contains no #f, thus member returns #f.
+    (if (false? (member #f header))
         (parse-headers in
-                       (cons (cons (second header) (third header))
-                             header-list)))))
+                       (cons (cons (fourth header) (fifth header))
+                             header-list))
+        header-list)))
 
 ;; Parse the HTTP request as composed by a request line and a head list.
 (define (parse-http-request in)
